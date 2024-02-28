68f8515bdc6360a572401c6924bf14df2c293edc
kiwizacxs <brian.deleon2016@outlook.com>
2024-02-28 16:41:45 +0000

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -13,16 +13,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialises a new instance or sets attributes based on kwargs"""
         if kwargs:
-
             for key, value in kwargs.items():
                 if key in ('created_at', 'updated_at'):
-
-                    value = datetime.strptime(value, format)
-
                     value = datetime.now()
                 if key != '__class__':
                     setattr(self, key, value)
-
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
