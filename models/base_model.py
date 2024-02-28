fc8c3a4dfccc55ba946d4cecb6ab6ae51600269c
Diego29012 <dieperey598@gmail.com>
2024-02-27 18:02:28 -0800

    add some code to base model

---
+++ models/base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+BaseModel class for AirBnB project.
+"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
