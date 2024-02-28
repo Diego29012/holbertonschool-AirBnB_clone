1cf8f7b0739ff695018475a132d1a7e8fdd11a6c
Diego29012 <dieperey598@gmail.com>
2024-02-28 00:52:16 -0800

    damn

--- models/base_model.py
+++ models/base_model.py
@@ -7,36 +7,18 @@ import uuid
 
 
 class BaseModel:
-    """"
-    BaseModel class that defines common attributes/methods for other classes
-    """
-    def __init__(self, *args, **kwargs):
-        """Initialises a new instance or sets attributes based on kwargs"""
-        format = "%Y-%m-%dT%H:%M:%S.%f"
-
-        if kwargs:
-
-            for key, value in kwargs.items():
-                if key in ('created_at', 'updated_at'):
-                    value = datetime.strptime(value, format)
-                if key != '__class__':
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """
-        Updates date and time of the instance and saves to file storage
-        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        self.save()
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
