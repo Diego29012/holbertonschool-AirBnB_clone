c32e315695d1e482c6d16dbb59b19b0739767976
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-28 14:55:12 +0000

    add kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@ BaseModel class for AirBnB from datetime import datetime
 """
 from datetime import datetime
 import uuid
-from models import storage
 
 
 class BaseModel:
@@ -17,14 +16,13 @@ class BaseModel:
 
             for key, value in kwargs.items():
                 if key in ('created_at', 'updated_at'):
-                    value = datetime.strptime(value, format)
+                    value = datetime.now()
                 if key != '__class__':
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         """
