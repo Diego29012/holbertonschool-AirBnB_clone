c481063d77e6438aa9c15a87bb5d3cc97ef1b50d
Brian De León <124705981+Kiwizacxs@users.noreply.github.com>
2024-02-28 12:36:46 -0300

    Update file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+FileStorage
+"""
+
+
+class FileStorage():
+    """
+    That serializes instances to a JSON file and deserializes 
+    JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+
+    def all(self):
+        """
+        Returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+         sets in __objects the obj with key <obj class name>.id
+        """
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path)
+        """
+
+    def reload():
+        """
+        deserializes the JSON file to __objects
+        (only if the JSON file (__file_path)exists;
+        otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        """
