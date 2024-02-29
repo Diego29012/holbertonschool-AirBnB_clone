163e79ba822fbd46a66c828bdc527365ba300d0b
Kiwizacxs <brian.deleon2016@outlook.com>
2024-02-29 22:11:51 +0000

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -21,8 +21,8 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            self.created_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
@@ -46,7 +46,7 @@ class BaseModel:
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['id'] = self.id
         return obj_dict
