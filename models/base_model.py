710e65322f8793e8302a6b42f4ea47e76e785eeb
Kiwizacxs <bian.deleon2016@outlook.com>
2024-03-03 19:20:08 +0000

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -47,6 +47,6 @@ class BaseModel:
         obj_dict = self.__dict__.copy()
         obj_dict['id'] = self.id
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['updated_at'] = self.updated_at.isoformat()
         obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
