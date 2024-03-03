07419d91a5d094076e0b7ee46f5e9c9a9a30091e
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-03-03 18:59:09 -0300

    add the city.py

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The Module for City class"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Defines the attributes for the City class"""
+
+    state_id = ""
+    name = ""
