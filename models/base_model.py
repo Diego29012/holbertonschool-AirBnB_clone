5f43bb8772673698317a1d8a609d87035f8e124a
Kiwizacxs <bian.deleon2016@outlook.com>
2024-03-03 18:44:28 +0000

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -49,9 +49,8 @@ class BaseModel:
             if key != '__class__'
         }
 
-        obj_dict = self.__dict__.copy()
+        obj_dict['id'] = self.id
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['updated_at'] = self.updated_at.isoformat()
         obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['id'] = self.id
         return obj_dict
